d27e2419bced34fc8e2203db265df34c6371292d
erangeljr <hacker@eddierangel.com>
2014-06-07 22:55:34 -0700

    Creating new Layout
    manual.axml

--- HealthTracker/Resources/Resource.designer.cs
+++ HealthTracker/Resources/Resource.designer.cs
@@ -97,6 +97,9 @@ namespace HealthTracker
 			// aapt resource value: 0x7f030001
 			public const int Main = 2130903041;
 			
+			// aapt resource value: 0x7f030002
+			public const int Manual = 2130903042;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
--- HealthTracker/obj/Debug/android/healthtracker/healthtracker/R.java
+++ HealthTracker/obj/Debug/android/healthtracker/healthtracker/R.java
@@ -24,6 +24,7 @@ public final class R {
     public static final class layout {
         public static final int goals=0x7f030000;
         public static final int main=0x7f030001;
+        public static final int manual=0x7f030002;
     }
     public static final class string {
         public static final int Enter=0x7f040004;
